d5ae88cfee5866be02f206edb833ae1e6dea2b65
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-12-27 18:53:38 +0100

    handled disabled button + clear fields on add

--- components/Quotes/index.tsx
+++ components/Quotes/index.tsx
@@ -2,7 +2,8 @@
 
 import { storedQuotes } from "@/utils/storage";
 import { Quote } from "@/utils/types";
-import { ChangeEvent, useState } from "react";
+import clsx from "clsx";
+import { ChangeEvent, useMemo, useState } from "react";
 
 export default function Quotes() {
 
@@ -13,12 +14,15 @@ export default function Quotes() {
 
   const [allQuotes, setAllQuotes] = useState<Quote[]>(storedQuotes.getAll())
 
+  const isButtonDisabled = useMemo(() => quoteData.quote.trim() === '', [quoteData.quote])
+
   const addQuote = () => {
     const newQuote = {
       author: quoteData.author, quote: quoteData.quote
     }
     setAllQuotes(prev => ([...prev, newQuote]))
     storedQuotes.add(newQuote)
+    setQuoteData({author: '', quote: ''})
   }
 
   const handleChange = (field: 'author' | 'quote') => (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -35,7 +39,7 @@ export default function Quotes() {
         <input type="text" name="author" id="author" className="border-2 rounded-lg border-grey" value={quoteData.author} onChange={handleChange('author')} />
       </div>
       <textarea value={quoteData.quote} className="border-2 rounded-lg p-2 border-grey" onChange={handleChange('quote')}></textarea>
-      <button className="bg-sky-300 rounded-lg p-1 hover:opacity-80" onClick={addQuote}>Add new quote</button>
+      <button className={clsx("rounded-lg p-1 ", isButtonDisabled ? 'bg-gray-400 text-white' : 'bg-sky-300 text-black hover:opacity-80')} onClick={addQuote} disabled={isButtonDisabled}>Add new quote</button>
 
       {allQuotes.map((item: Quote, i: number) => (
         <div key={`quote-${i}`}>

5caf9c2069a9524479c03222a6820eeeb088ebf1
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-12-27 23:49:53 +0100

    style fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Quotes from "@/components/Quotes";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen font-sans p-6">
+    <main className="flex flex-col md:items-center md:justify-center min-h-screen font-sans p-6">
       <section className="flex flex-col gap-4 md:max-w-60 w-full">
         <h1 className="text-3xl font-bold">Quotes</h1>
         <Quotes />

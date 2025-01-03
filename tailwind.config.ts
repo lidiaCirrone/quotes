68ce972f28658fa7aedcd924efa4f916e4a0604f
Lidia Cirrone <lidia.cirrone@gmail.com>
2025-01-03 21:50:06 +0100

    installed @nextui-org/spinner

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/modal.js"
+    "./node_modules/@nextui-org/theme/dist/components/(modal|spinner).js"
   ],
   theme: {
     fontFamily: {

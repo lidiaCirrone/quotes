55caf9a833dfb521af6736916e8744a693fc31e0
Lidia Cirrone <lidia.cirrone@gmail.com>
2025-01-05 16:19:45 +0100

    fixed no quotes message if array is empty + style fixes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex flex-col gap-4 min-h-screen sm:max-h-screen font-sans p-6 sm:max-w-[90vw] lg:max-w-[70vw] sm:mx-auto">
       <h1 className="text-3xl font-bold">Quotes</h1>
       <section className="flex flex-col sm:flex-row min-h-screen sm:min-h-0 gap-8 w-full sm:max-h-full sm:overflow-y-hidden">
-        <div className="flex flex-col gap-4 sm:flex-1 sm:overflow-y-auto sm:pr-2">
+        <div className="flex flex-col gap-4 sm:flex-1 sm:overflow-y-auto sm:p-2">
           <RandomQuote />
           <AddQuoteForm />
         </div>
--- components/QuotesList/index.tsx
+++ components/QuotesList/index.tsx
@@ -55,27 +55,49 @@ export default function QuotesList() {
     <div className="flex flex-col gap-4 sm:flex-1 sm:max-h-full">
 
       {allQuotes.length > 0 ? (
-        <div className="flex items-center justify-end gap-2 sm:pr-2">
-          <label htmlFor="filter" className="shrink-0">Filter by: </label>
-          <input type="text" name="filter" id="filter" className="w-full border-2 rounded-lg border-grey" value={filter.current} onChange={onFilterChange} onKeyUp={handleKeyUp} />
-          <IconWrapper
-            disabled={filter.current.trim() === ""}
-            IconComponent={TbFilterCheck}
-            onClick={handleFilterApply}
-            tooltip={{
-              triggerId: "apply-filter-tooltip",
-              text: "Apply filter"
-            }}
-          />
-          <IconWrapper
-            IconComponent={TbFilterX}
-            onClick={handleFilterRemove}
-            tooltip={{
-              triggerId: "remove-filter-tooltip",
-              text: "Remove filter"
-            }}
-          />
-        </div>
+        <>
+          <div className="flex items-center justify-end gap-2 sm:p-2">
+            <label htmlFor="filter" className="shrink-0">Filter by: </label>
+            <input type="text" name="filter" id="filter" className="w-full border-2 rounded-lg border-grey" value={filter.current} onChange={onFilterChange} onKeyUp={handleKeyUp} />
+            <IconWrapper
+              disabled={filter.current.trim() === ""}
+              IconComponent={TbFilterCheck}
+              onClick={handleFilterApply}
+              tooltip={{
+                triggerId: "apply-filter-tooltip",
+                text: "Apply filter"
+              }}
+            />
+            <IconWrapper
+              IconComponent={TbFilterX}
+              onClick={handleFilterRemove}
+              tooltip={{
+                triggerId: "remove-filter-tooltip",
+                text: "Remove filter"
+              }}
+            />
+          </div>
+
+          <div className="flex flex-col gap-4 sm:overflow-y-auto sm:p-2">
+            {filteredQuotes.length > 0 ? filteredQuotes.sort((a, b) => b.date - a.date).map((item: Quote, i: number) => (
+              <div key={`quote-${i}`} className="flex gap-2">
+                <QuoteCard author={item.author} text={item.quote}
+                />
+                <IconWrapper
+                  wrapperClassName="h-fit mt-3"
+                  IconComponent={TbCopy}
+                  onClick={handleCopy(item)}
+                  tooltip={{
+                    triggerId: "copy-quote-tooltip",
+                    text: "Copy this quote"
+                  }}
+                />
+              </div>
+            )) :
+              <p className="bg-gray-100 p-2 rounded-lg text-center">No quote matches that filter</p>
+            }
+          </div>
+        </>
       ) : (
         <div className="sm:flex sm:items-center sm:justify-center sm:w-full sm:h-full">
           <p className="sm:bg-gray-100 sm:p-6 sm:rounded-lg sm:text-center sm:max-w-[50%]">
@@ -83,26 +105,6 @@ export default function QuotesList() {
           </p>
         </div>
       )}
-
-      <div className="flex flex-col gap-4 sm:overflow-y-auto sm:pr-2">
-        {filteredQuotes.length > 0 ? filteredQuotes.sort((a, b) => b.date - a.date).map((item: Quote, i: number) => (
-          <div key={`quote-${i}`} className="flex gap-2">
-            <QuoteCard author={item.author} text={item.quote}
-            />
-            <IconWrapper
-              wrapperClassName="h-fit mt-3"
-              IconComponent={TbCopy}
-              onClick={handleCopy(item)}
-              tooltip={{
-                triggerId: "copy-quote-tooltip",
-                text: "Copy this quote"
-              }}
-            />
-          </div>
-        )) :
-          <p className="bg-gray-100 p-2 rounded-lg text-center">No quote matches that filter</p>
-        }
-      </div>
     </div>
   );
 }
